18f988adcaf7fcdd11eaf29333373b63b391dca4
maksim.av <exemplleer@gmail.com>
2023-01-29 16:04:17 +0300

    Fix style

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ export const isEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
 export const getRandomNumber = () => Math.round(Math.random() * 100);
 
 export const getRandomOperator = () => {
-  const ranodomNumber = (Math.round(Math.random() * 10) % 3) + 1;
-  switch (ranodomNumber) {
+  const randomNumber = (Math.round(Math.random() * 10) % 3) + 1;
+  switch (randomNumber) {
     case 1:
       return '+';
     case 2:
