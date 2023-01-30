39e3103a2aefe7ea318681907f9e1878a0134468
maksim.av <exemplleer@gmail.com>
2023-01-30 22:40:44 +0300

    Fix brain-games logic

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,12 +1,4 @@
 #!/usr/bin/env node
 import greeting from '../src/cli.js';
-import evenGame from '../src/games/even.js';
-import calcGame from '../src/games/calc.js';
-import gcdGame from '../src/games/gcd.js';
-import progressionGame from '../src/games/progression.js';
 
 greeting();
-evenGame();
-calcGame();
-gcdGame();
-progressionGame();
