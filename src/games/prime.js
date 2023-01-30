60a22af5a6400393c6247005c43e1050fcc9a2c9
maksim.av <exemplleer@gmail.com>
2023-01-30 22:25:24 +0300

    Add feature brain-prime

---
+++ src/games/prime.js
@@ -0,0 +1,34 @@
+import {
+  win, fail, correct, questionAndInput,
+} from '../cli.js';
+
+import { getRandomNumber, roundsCount } from '../index.js';
+
+const isPrime = (num) => {
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) {
+      return 'no';
+    }
+  }
+  return 'yes';
+};
+
+const primeGame = () => {
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+  for (let i = 0; i < roundsCount; i += 1) {
+    const randomNumber = getRandomNumber();
+
+    const answer = isPrime(randomNumber);
+    const userInput = questionAndInput(randomNumber).toLowerCase();
+
+    if (userInput === answer) {
+      correct();
+    } else {
+      return fail(userInput, answer);
+    }
+  }
+  return win();
+};
+
+export default primeGame;
